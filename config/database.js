5c9ce60d56870bdcb606a7829b721ab8199cf378
John Beynon <john@render.com>
2022-05-26 14:20:26 +0000

    Change to connection string for PG

--- config/database.js
+++ config/database.js
@@ -1,13 +1,33 @@
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'postgres',
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'postgres',
+//     connection: {
+//       host: env('DATABASE_HOST', '127.0.0.1'),
+//       port: env.int('DATABASE_PORT', 5432),
+//       database: env('DATABASE_NAME', 'workspace'),
+//       user: env('DATABASE_USERNAME', 'postgres'),
+//       password: env('DATABASE_PASSWORD', 'postgres'),
+//       ssl: env.bool('DATABASE_SSL', false),
+//     },
+//   },
+// });
+
+const { parse } = require("pg-connection-string");
+
+module.exports = ({ env }) => {
+  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+
+  return {
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'workspace'),
-      user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', 'postgres'),
-      ssl: env.bool('DATABASE_SSL', false),
+      client: "postgres",
+      connection: {
+        host,
+        port,
+        database,
+        user,
+        password
+      },
+      debug: false,
     },
-  },
-});
+  };
+};
